4cc430decca0e2780fd1265c6eb65b6854543331
Michael Sommer <m.sommer@ogd.nl>
2012-08-24 11:30:00 +0200

    De verbindingen hebben nu de kleur van het VLAN

---
+++ Source/Domain/Models/InfectionColor.cs
@@ -0,0 +1,10 @@
+﻿namespace Ogd.Virarium.Domain.Models
+{
+    public enum InfectionColor
+    {
+        Green = 0,
+        Yellow = 1,
+        Orange = 5,
+        Red = 10
+    }
+}
---
+++ Source/Domain/Models/VLanColor.cs
@@ -0,0 +1,16 @@
+﻿namespace Ogd.Virarium.Domain.Models
+{
+    public enum VLanColor
+    {
+        Black = 0,
+        Red = 1,
+        Blue = 2,
+        Green = 3,
+        White = 4,
+        Yellow = 5,
+        Purple = 7,
+        Magenta = 8,
+        Fuchsia = 9,
+        Lime = 10
+    }
+}
